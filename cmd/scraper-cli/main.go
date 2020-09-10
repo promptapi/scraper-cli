95e66efb0163df9705faaa8f738dc6b209df28ab
Uğur Özyılmazel <ugur@promptapi.com>
2020-09-10 08:03:36 +0300

    Add basic cmd flags

--- cmd/scraper-cli/main.go
+++ cmd/scraper-cli/main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	cmd := app.NewCLIApplication()
+
 	if err := cmd.Run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,9 +1,28 @@
 package app
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"os"
+)
+
+const (
+	version = "0.0.0"
+)
+
+var (
+	optVersionInformation *bool
+
+	usage = `
+Usage: scraper-cli [options...]
+
+@wip
+
+Options:
+
+  -h, --help            Display help :)
+`
 )
 
 // CLIApplication represents app structure
@@ -13,6 +32,13 @@ type CLIApplication struct {
 
 // NewCLIApplication creates new CLIApplication instance
 func NewCLIApplication() *CLIApplication {
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, usage)
+	}
+	optVersionInformation = flag.Bool("version", false, "Current version information")
+
+	flag.Parse()
+
 	return &CLIApplication{
 		Out: os.Stdout,
 	}
@@ -20,6 +46,16 @@ func NewCLIApplication() *CLIApplication {
 
 // Run executes main application
 func (c *CLIApplication) Run() error {
+	if *optVersionInformation {
+		return c.Version()
+	}
+
 	fmt.Fprintf(c.Out, "Hello world")
+	return nil
+}
+
+// Version returns the current version of CLIApplication
+func (c *CLIApplication) Version() error {
+	fmt.Fprintf(c.Out, "%s\n", version)
 	return nil
 }
